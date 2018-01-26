bfe1d9ab0d9b9a29bdfab6e22ebc836dce12923c
CyreneJaneDispo <cyrenejanedispo132@gmail.com>
2018-01-26 23:35:29 +0800

    with additional data

--- wifira.sql
+++ wifira.sql
@@ -2,7 +2,7 @@ CREATE DATABASE  IF NOT EXISTS `wifira` /*!40100 DEFAULT CHARACTER SET latin1 */
 USE `wifira`;
 -- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: wifira
+-- Host: localhost    Database: wifira
 -- ------------------------------------------------------
 -- Server version	5.7.11
 
@@ -32,7 +32,7 @@ CREATE TABLE `accounts` (
   `username` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
   `accountStatus` enum('Enable','Disable') NOT NULL,
-  `image` longblob NOT NULL,
+  `image` longblob,
   PRIMARY KEY (`accountNo`),
   KEY `accountNo` (`accountNo`),
   KEY `accoun_no` (`accountNo`)
@@ -45,6 +45,7 @@ CREATE TABLE `accounts` (
 
 LOCK TABLES `accounts` WRITE;
 /*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
+INSERT INTO `accounts` VALUES (1,'Staff','Alfa Leizel Leones','Baguio City','alfa123','000','Enable',NULL),(2,'Staff','Joneil Argao','Zambales','joneil123','111','Enable',NULL),(3,'Staff','James Anonuevo','Baguio City','james123','222','Disable',NULL),(4,'Staff','Cyrene Jane Dispo','Pangasinan','cyrene123','333','Disable',NULL),(5,'Admin','Lou Philip Beltran','Baguio City','lou123','444','Enable',NULL),(6,'Staff','Katherine Turqueza','Abra','kath123','555','Enable',NULL),(7,'Staff','Maureen Nicole Calpito','Baguio City','mau123','666','Enable',NULL),(8,'Staff','Apollo Joshua Mina','Baguio City','ajm123','777','Enable',NULL),(9,'Admin','Madonna Beltran','Baguio City','pido123','888','Enable',NULL),(10,'Admin','Don Lopen Beltran','Baguio City','Lopen123','999','Enable',NULL);
 /*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -73,6 +74,7 @@ CREATE TABLE `kiosk machine` (
 
 LOCK TABLES `kiosk machine` WRITE;
 /*!40000 ALTER TABLE `kiosk machine` DISABLE KEYS */;
+INSERT INTO `kiosk machine` VALUES (11,'FSGS','SLU Main','192.168.9.2','Enable'),(12,'ASDF','Bakakeng','192.168.9.1','Enable'),(13,'SDFF','UB','192.168.9.3','Enable'),(14,'SCFG','Session','192.168.9.4','Enable'),(15,'WSCF','Burnham','192.168.9.5','Enable');
 /*!40000 ALTER TABLE `kiosk machine` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -86,10 +88,10 @@ DROP TABLE IF EXISTS `sales`;
 CREATE TABLE `sales` (
   `salesId` int(11) NOT NULL,
   `entity` enum('ADMIN','STAFF','KIOSK') NOT NULL,
-  `voucherCode` int(11) NOT NULL,
+  `voucherCode` varchar(45) NOT NULL,
   `dateUsed` date NOT NULL,
-  `accountNo` int(11) NOT NULL,
-  `kioskId` int(11) NOT NULL,
+  `accountNo` int(11) DEFAULT NULL,
+  `kioskId` int(11) DEFAULT NULL,
   PRIMARY KEY (`salesId`),
   KEY `voucherCode_idx` (`voucherCode`),
   KEY `kioskId_idx` (`kioskId`),
@@ -107,6 +109,7 @@ CREATE TABLE `sales` (
 
 LOCK TABLES `sales` WRITE;
 /*!40000 ALTER TABLE `sales` DISABLE KEYS */;
+INSERT INTO `sales` VALUES (123,'STAFF','10082-12312','2017-10-11',1,NULL),(134,'ADMIN','26856-68924','2018-01-25',9,NULL),(153,'STAFF','78945-25642','2018-01-26',1,NULL),(234,'STAFF','10045-45234','2017-10-16',2,NULL),(347,'STAFF','53628-37287','2017-10-19',6,NULL),(457,'ADMIN','12672-34566','2017-10-18',10,NULL),(513,'ADMIN','12349-56812','2018-01-15',9,NULL),(567,'ADMIN','12345-67809','2017-10-17',10,NULL),(568,'STAFF','25698-15632','2018-01-18',4,NULL),(682,'STAFF','12569-12356','2018-01-17',6,NULL);
 /*!40000 ALTER TABLE `sales` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -118,13 +121,13 @@ DROP TABLE IF EXISTS `vouchers`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `vouchers` (
-  `voucherCode` int(11) NOT NULL,
+  `voucherCode` varchar(45) NOT NULL,
   `voucherType` enum('A','B','C') NOT NULL,
   `voucherAmount` enum('10','20','100') NOT NULL,
   `voucherStatus` enum('Sold','Unsold') NOT NULL,
   `datePrinted` date NOT NULL,
-  `accountNo` int(11) NOT NULL,
-  `kioskId` int(11) NOT NULL,
+  `accountNo` int(11) DEFAULT NULL,
+  `kioskId` int(11) DEFAULT NULL,
   PRIMARY KEY (`voucherCode`),
   KEY `accountNo_idx` (`accountNo`),
   KEY `voucherCode` (`voucherCode`),
@@ -140,6 +143,7 @@ CREATE TABLE `vouchers` (
 
 LOCK TABLES `vouchers` WRITE;
 /*!40000 ALTER TABLE `vouchers` DISABLE KEYS */;
+INSERT INTO `vouchers` VALUES ('10045-45234','A','10','Sold','2017-10-12',2,NULL),('10082-12312','A','10','Sold','2017-10-11',1,NULL),('12345-67809','B','20','Unsold','2017-10-14',2,NULL),('12349-56812','B','20','Sold','2017-12-15',9,NULL),('12569-12356','C','100','Sold','2017-12-26',1,15),('12672-34566','B','20','Sold','2017-10-13',1,NULL),('25698-15632','A','10','Sold','2018-01-15',1,NULL),('26856-68924','A','10','Unsold','2017-12-25',5,NULL),('53628-37287','A','10','Unsold','2017-10-15',2,NULL),('78945-25642','B','20','Sold','2018-01-19',2,NULL);
 /*!40000 ALTER TABLE `vouchers` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -152,4 +156,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-01-26 14:44:11
+-- Dump completed on 2018-01-26 23:13:38
