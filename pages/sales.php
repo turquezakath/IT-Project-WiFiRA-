82955770662e472220a1d922c307ab43fe1463f3
joneilargao <joneilargao.ja@gmail.com>
2018-01-27 04:16:15 +0800

    Added dropdown menu with functionalities

--- pages/sales.php
+++ pages/sales.php
@@ -38,12 +38,32 @@ require '../classes/UserAccount.php';
                 <div class="row">
                     <div class="col-md-12">
                         <h1 style = "font-family: special elite; color:#000000">Sales</h1>
-                        
+
     <form id="search-form" name="search" action="" method="get">
     <input id="search-input" name="search" type="text">
     <input type="submit" name='submit' class="btn btn-warning" value="Search" class="col s6" class='submit' style="background-color:#686667; font-family:monospace; font-size:18px;"/>
+
+                    <form action="sales.php" method="get">
+                        <select name="user">
+                            <option value="">Choose Entity</option>
+                            <?php 
+                                require_once 'fragments/connection.php';
+                                $usersQuerry = $pdo->prepare("SELECT name FROM wifira.accounts  union SELECT kioskName FROM wifira.`kiosk machine`;");
+                                $usersQuerry->execute();
+                                $users = $usersQuerry->fetchAll();
+                            foreach ($users as $user){
+                                echo "<option>" . $user['name'] . "</option>";
+                            }
+                            ?>
+                        </select>
+                    </form>
+
+                    
+
+
                     </div>    
                 </div>
+
                 <div class="jumbotron"> 
                     <table class="table table-striped table-bordered table-hover" id="dataTables-example" name="anothercontent">
                         <?php
