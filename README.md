7f2e6908480fd3ebf5a16f6703d68c72ae7d32fe
joneilargao <joneilargao.ja@gmail.com>
2018-02-19 14:36:41 +0800

    Added and move files

--- README.md
+++
@@ -1,3 +0,0 @@
-Guys please post here the updates on our Sales Monitoring system.
-
-Please update first before commit.
--- pages/fragments/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	try{
-		$pdo = new PDO("mysql:host=localhost;dbname=wifira","root","");
-	} catch (PDOException $e) {
-		exit("Error: Could not establish connection to database.");
-	}
-?>
