14c0e5794029bf6dacf18775607e4a42d67a61c7
joneilargao <joneilargao.ja@gmail.com>
2018-02-22 22:50:23 +0800

    try css

--- old/login.php
+++ old/login.php
@@ -9,7 +9,7 @@ require 'classes/UserAccount.php';
       <title>WiFiRA ISP</title><meta charset="UTF-8" />
 
       <link rel="stylesheet" type="text/css" href="pages/assets/css/style.css"/>
-      <link rel="stylesheet" type="text/css" href="pages/assets/css/style2.css"/>
+      <link rel="stylesheet" type="text/css" href="css/style2.css"/>
 
     
 </head>
