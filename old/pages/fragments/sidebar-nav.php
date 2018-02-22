e92c95d03b34731af259168e498d60e15eefedcc
turquezakath <turquezakath@gmail.com>
2018-02-23 00:15:32 +0800

    Edited page-head.php and sidebar-nav.php

--- old/pages/fragments/sidebar-nav.php
+++ old/pages/fragments/sidebar-nav.php
@@ -9,59 +9,60 @@
 ?>
 <html>
     <head>
-        <link href="https://fonts.googleapis.com/css?family=Allura|Arima+Madurai|Cinzel+Decorative|Corben|Dancing+Script|Galindo|Gentium+Book+Basic|Great+Vibes|Henny+Penny|Indie+Flower|Kaushan+Script|Kurale|Life+Savers|Love+Ya+Like+A+Sister|Milonga|Miltonian+Tattoo|Niconne|Oregano|Original+Surfer|Pangolin|Parisienne|Philosopher|Princess+Sofia|Rancho|Risque|Salsa|Schoolbell|Special+Elite" rel="stylesheet">		
+        <link href="https://fonts.googleapis.com/css?family=Allura|Arima+Madurai|Cinzel+Decorative|Corben|Dancing+Script|Galindo|Gentium+Book+Basic|Great+Vibes|Henny+Penny|Indie+Flower|Kaushan+Script|Kurale|Life+Savers|Love+Ya+Like+A+Sister|Milonga|Miltonian+Tattoo|Niconne|Oregano|Original+Surfer|Pangolin|Parisienne|Philosopher|Princess+Sofia|Rancho|Risque|Salsa|Schoolbell|Special+Elite" rel="stylesheet">	
+        <link rel="stylesheet" type="text/css" href="../assets/css/nav.css">
     </head>
-<nav class="navbar-default navbar-side" role="navigation">
-    <div class="sidebar-collapse">
-        <ul class="nav" id="main-menu">
-
-        <li style ="font-family:Cinzel Decorative; font-size:18px">
+<nav class="navbar-default navbar-side" role="navigation" >
+    <div class="sidebar-collapse" style="background-color: #E95819;">
+        <ul class="nav" id="main-menu" style="position: fixed;" >
+    
+        <li>
             <a href=index.php><i class="fa fa-tachometer"></i> Dashboard<span class="fa arrow"></span></a>
         </li>
-        <li style ="font-family: Cinzel Decorative; font-size:18px">
+        <li>
             <a href="sales.php"><i class="fa fa-usd "></i> Sales<span class="fa arrow"></span></a>
             <ul class="nav nav-second-level">
-                 <li style ="font-family: Cinzel Decorative;">
-                    <a  href="sales-daily.php"><i class="fa fa-usd "></i>Daily</a>
+                 <li>
+                    <a  href="sales-daily.php"><i class="fa fa-calendar-o "></i>Daily</a>
                 </li>
-                <li style ="font-family: Cinzel Decorative;">
-                    <a  href="sales-weekly.php"><i class="fa fa-usd "></i>Weekly</a>
+                <li>
+                    <a  href="sales-weekly.php"><i class="fa fa-calendar "></i>Weekly</a>
                 </li>
-                <li style ="font-family: Cinzel Decorative;">
-                    <a  href="sales-monthly.php"><i class="fa fa-usd "></i>Monthly</a>
+                <li>
+                    <a  href="sales-monthly.php"><i class="fa fa-calendar "></i>Monthly</a>
                 </li>
-                <li style ="font-family: Cinzel Decorative;">
-                    <a  href="sales-yearly.php"><i class="fa fa-usd "></i>Yearly</a>
+                <li>
+                    <a  href="sales-yearly.php"><i class="fa fa-calendar "></i>Yearly</a>
                 </li>
             </ul>				
         </li>
 
-        <li style ="font-family: Cinzel Decorative; font-size:18px">
+        <li>
             <a href="#"><i class="fa fa-barcode"></i>Kiosks<span class="fa arrow"></span></a>
 			<ul class="nav nav-second-level">
-            	<li style ="font-family: Cinzel Decorative;">
+            	<li>
                     <a  href="kiosk-manage.php"><i class="fa fa-barcode"></i>Manage Kiosks</a>
                 </li>
-				<li style ="font-family: Cinzel Decorative;">
+				<li>
                     <a  href="kiosk-add.php"><i class="fa fa-barcode"></i>Add Kiosk</a>
                 </li>
             </ul>
         </li>
-        <li  style ="font-family: Cinzel Decorative;">
-            <a href="#"  style ="font-family: Cinzel Decorative; font-size:18px"><i class="fa fa-user" ></i> Accounts<span class="fa arrow"></span></a>
+        <li>
+            <a href="#"><i class="fa fa-user" ></i> Accounts<span class="fa arrow"></span></a>
             <ul class="nav nav-second-level">
-                <li style ="font-family: Cinzel Decorative;">
-                    <a  href="view-profile.php"><i class="fa fa-user fa-3x"></i> View Profile</a>
+                <li>
+                    <a  href="view-profile.php"><i class="fa fa-user fa-2x"></i> View Profile</a>
                 </li>
-                <li style ="font-family: Cinzel Decorative;">
-                                    <a  href="view-staff-profile.php"><i class="fa fa-user fa-3x"></i>Manage Staff Accounts</a>
-                                </li>
-                <li style ="font-family: Cinzel Decorative;">
-                    <a href="edit-profile.php"><i class="fa fa-pencil-square fa-3x"></i> Edit Profile</a>
+                <li>
+                    <a  href="view-staff-profile.php"><i class="fa fa-user fa-2x"></i>Manage Staff Accounts</a>
+                </li>
+                <li>
+                    <a href="edit-profile.php"><i class="fa fa-pencil-square fa-2x"></i> Edit Profile</a>
                 </li>
             </ul>            
         </li>
-        <li style ="font-family: Cinzel Decorative;">
+        <li>
             
         </li>			
         </ul>
